6a5b71afa845f215eee6a1c2b95579444f1ed761
pyscripter <pyscripter@gmail.com>
2020-08-21 12:46:49 +0300

    Fixes #51 Fixes #50

--- Svg/SVGColor.pas
+++ Svg/SVGColor.pas
@@ -205,6 +205,11 @@ begin
     Result := SVG_INHERIT_COLOR;
     Exit;
   end;
+  if SameText(ASVGColorName, 'currentColor') then
+  begin
+    Result := TColors.Black;
+    Exit;
+  end;
 
   Index := SVGColorList.IndexOf(ASVGColorName);
   if Index >= 0 then
